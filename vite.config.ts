de905a141455f46c4c761c5050eb389fb46d721a
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2025-01-29 02:56:08 +0100

    Build outdir

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    outDir: './pocketbase/pb_public'
-  }
+  // build: {
+  //   outDir: './pocketbase/pb_public'
+  // }
 })
